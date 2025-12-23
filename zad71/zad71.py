4a8b34afa04835b6706a8e1f70d531353c4e41ae
ryszarddddl <ryszarddddl@gmail.com>
2025-12-23 19:25:55 +0100

    Update zad71.py

--- zad71/zad71.py
+++ zad71/zad71.py
@@ -193,7 +193,7 @@ else:
         if 'json_cluster_names_and_descriptions' not in st.session_state:
             st.session_state.json_cluster_names_and_descriptions = None
         if st.session_state.json_cluster_names_and_descriptions is None:
-            st.session_state.api_key = handle_openai_key()
+            api_key = handle_openai_key()
             lista_json = [f.name for f in current_dir.glob("*.json")]
             #lista_pkl = [f.name for f in current_dir.iterdir() if f.is_file() and f.suffix == ".json"]
             #if lista_pkl:
@@ -212,7 +212,7 @@ else:
                 submit_button = st.form_submit_button("Zatwierdź nazwę opisu modelu")
                 if not submit_button:
                     st.stop()
-                make_descriptions(st.session_state.d_model,st.session_state.data_df,CLUSTER_NAMES_AND_DESCRIPTIONS,st.session_state.api_key)
+                make_descriptions(st.session_state.d_model,st.session_state.data_df,CLUSTER_NAMES_AND_DESCRIPTIONS,api_key)
                 st.write('Skończyłem generowanie opisów do modelu treningowego')
                 st.session_state.json_cluster_names_and_descriptions = get_cluster_names_and_descriptions(CLUSTER_NAMES_AND_DESCRIPTIONS)
             if st.button("OK"):
